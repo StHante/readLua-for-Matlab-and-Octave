9692cea127024c542e3793b1585792030abff769
Stefan Hante <stefan@hante.de>
2016-04-29 12:11:06 +0200

    Use string.h for memcpy, declarare function because of mutually calling functions

--- readLua.c
+++ readLua.c
@@ -1,6 +1,9 @@
 #include "mex.h"
 /*#include "matrix.h"*/
 
+/* Needed for memcpy */
+#include <string.h>
+
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
@@ -19,23 +22,23 @@ double my_lua_get_and_pop_number(lua_State* L) {
    lua_pop(L, 1);
    return val;
 }
-   
+
 const char* my_lua_get_and_pop_string(lua_State* L, size_t* string_len) {
    if (!lua_isstring(L, -1)) {
       lua_close(L);
       mexErrMsgIdAndTxt("readLua:LuaError",
               "Top of lua stack should be a string, but is not.");
    }
-   
+
    /* Get string and its length */
    const char* myval = lua_tolstring(L, -1, string_len);
-   
+
    /* Allocate memory */
    char* val = (char*) mxCalloc(*string_len+1, sizeof(char));
-   
+
    /* Copy string */
    memcpy(val, myval, (*string_len+1) * sizeof(char));
-   
+
    lua_pop(L, 1);
    return (const char*) val;
 }
@@ -45,7 +48,7 @@ double* my_lua_get_and_pop_vector(lua_State* L, size_t* array_len) {
       lua_close(L);
       mexErrMsgIdAndTxt("readLua:LuaError",
               "Top of lua stack should be a table, but is not.");
-   }   
+   }
    /* Get size */
    lua_len(L, -1);
    *array_len = (size_t) my_lua_get_and_pop_number(L);
@@ -61,10 +64,10 @@ double* my_lua_get_and_pop_vector(lua_State* L, size_t* array_len) {
       lua_gettable(L, -2);
       val[i] = my_lua_get_and_pop_number(L);
    }
-   
+
    /* Make stack clean */
    lua_pop(L, 1);
-   
+
    return val;
 }
 
@@ -84,9 +87,9 @@ void checkForValidVariableName(const char* name) {
       } else {
          mexErrMsgIdAndTxt( "readLua:invalidVariableName", /* TODO */
             "The name %s is not a valid Matlab variable Name.", name);
-      }  
+      }
    }
-   
+
    /* Name is a valid */
    return;
 }
@@ -95,13 +98,13 @@ void addFieldToOut(mxArray* out,
                    const char* fieldname) {
    /* Check the fieldname */
    checkForValidVariableName(fieldname);
-   
+
    /* Add field to struct out */
    if (mxAddField(out, fieldname) == -1) {
       mexErrMsgIdAndTxt( "readLua:errorAddingField", /* TODO */
             "Error adding field to struct.");
    }
-   
+
    return;
 }
 
@@ -111,13 +114,13 @@ void addMatrixFieldToOut(mxArray* out,
                          size_t ncols,
                          double* Matrix) {
    mxArray* myMatrix;
-   
+
    /* Add field to struct out */
    addFieldToOut(out, fieldname);
-   
+
    /* Create myMatrix */
    myMatrix = mxCreateDoubleMatrix((mwSize) mrows, (mwSize) ncols, mxREAL);
-   
+
    /* Assign Matrix to myMatrix */
    mxSetPr(myMatrix, Matrix);
 
@@ -132,10 +135,10 @@ void addStringFieldToOut(mxArray* out,
                          size_t stringLen,
                          const char* string) {
    mxArray* myString;
-   
+
    /* Add field to struct out */
    addFieldToOut(out, fieldname);
-   
+
    /* Create myString */
    myString = mxCreateString(string);
 
@@ -145,16 +148,22 @@ void addStringFieldToOut(mxArray* out,
    return;
 }
 
+/* Declare function whose body is defined later */
+void myLuaGetTopAndAddFieldToOut(lua_State* L,
+                                 mxArray* out,
+                                 const char* key);
+
+
 void addStructFieldToOut(lua_State* L,
                          mxArray* out,
                          const char* key) {
    /* Create struct array */
    mxArray* strct;
    strct = mxCreateStructMatrix((mwSize) 1, (mwSize) 1, 0, NULL);
-   
+
    /* Add field to struct out */
    addFieldToOut(out, key);
-   
+
    /* Assign strct to out struct */
    mxSetField(out, (mwIndex) 0, key, strct);
 
@@ -172,7 +181,7 @@ void addStructFieldToOut(lua_State* L,
       /* Add the key-value-pair to the strct variable */
       myLuaGetTopAndAddFieldToOut(L, strct, lua_tostring(L, -2));
    }
-   
+
    return;
 }
 
@@ -184,16 +193,16 @@ void myLuaGetTopAndAddFieldToOut(lua_State* L,
    size_t arrayLen;
    const char* string;
    size_t stringLen;
-   
+
    /* Get the type of the variable on top of stack */
    ltype = lua_type(L, -1);
-   
+
    /* Decide what to do */
    if (ltype == LUA_TNUMBER) {
       /* Allocate space */
       array = (double*) mxCalloc((mwSize) 1, sizeof(double));
-      /* Get a number */      
-      array[0] = my_lua_get_and_pop_number(L); 
+      /* Get a number */
+      array[0] = my_lua_get_and_pop_number(L);
       /* A number is a matrix of format 1x1 */
       addMatrixFieldToOut(out, key, 1, 1, array);
    } else if (ltype == LUA_TTABLE) {
@@ -206,7 +215,7 @@ void myLuaGetTopAndAddFieldToOut(lua_State* L,
       /* Test if vector */
       if (lua_isnumber(L, -1)) {
          /* Pop value (key was popped by lua_gettable) */
-         lua_pop(L, 1);         
+         lua_pop(L, 1);
          /* We are dealing with a vector */
          array = my_lua_get_and_pop_vector(L, &arrayLen);
          /* A vector is a matrix of format 1xarrayLen */
@@ -214,10 +223,10 @@ void myLuaGetTopAndAddFieldToOut(lua_State* L,
       } else {
          /* Pop value (key was popped by lua_gettable) */
          lua_pop(L, 1);
-         
-         /* Table is treated as a record */         
+
+         /* Table is treated as a record */
          addStructFieldToOut(L, out, key);
-         
+
          /* Pop table */
          lua_pop(L, 1);
       }
@@ -235,7 +244,7 @@ void myLuaGetTopAndAddFieldToOut(lua_State* L,
       mexErrMsgIdAndTxt("readLua:UnsupportedType",
               "Lua variable %s has unsupported type.", key);
    }
-   
+
    return;
 }
 
@@ -244,10 +253,10 @@ void myLuaGetByKeyAndAddFieldToOut(lua_State* L,
                                    const char* key) {
    /* Put lua variable on top of stack */
    lua_getglobal(L, key);
-   
+
    /* Add the top of stack to out */
    myLuaGetTopAndAddFieldToOut(L, out, key);
-   
+
    return;
 }
 
@@ -261,12 +270,12 @@ void mexFunction( int nlhs, mxArray *plhs[],
    /* *******************************************************************
     * *** Checking i/o, preparing ***************************************
     * ******************************************************************/
-   
+
    size_t mrows, ncols;
    char* cfgName;
    size_t nSymbols;
    mxArray* out;
-   
+
    /* Check for proper number of arguments. */
    if(nrhs!=2) {
       mexErrMsgIdAndTxt( "readLua:invalidNumInputs", /* TODO */
@@ -283,13 +292,13 @@ void mexFunction( int nlhs, mxArray *plhs[],
       mexErrMsgIdAndTxt( "readLua:invalidInput", /* TODO*/
             "First input must be a nonempty string.");
    }
-   
+
    /* Get name of the cfg file */
    cfgName = (char*) mxCalloc(ncols+1, sizeof(char));
    if (mxGetString(prhs[0], cfgName, ncols+1) != 0)
       mexErrMsgIdAndTxt( "readLua:couldNotGetString", /* TODO */
               "Could not get string.");
-   
+
    /* The second input must be a nonempty cell array */
    mrows = mxGetM(prhs[1]);
    ncols = mxGetN(prhs[1]);
@@ -298,20 +307,20 @@ void mexFunction( int nlhs, mxArray *plhs[],
               "Second input must be a nonempty cell array.");
    }
    nSymbols = ncols;
-      
+
    /* Create output struct */
    out = mxCreateStructMatrix((mwSize) 1, (mwSize) 1, 0, NULL);
-   
+
    /* *******************************************************************
     * *** Opening Lua, reading file *************************************
     * ******************************************************************/
-   
+
    lua_State* L;
-   
+
    /* Open Lua */
    L = luaL_newstate();
    /* Open standard libraries */
-   luaL_openlibs(L); 
+   luaL_openlibs(L);
 
    /* Open file */
    if (luaL_loadfile(L, cfgName)) {
@@ -319,27 +328,27 @@ void mexFunction( int nlhs, mxArray *plhs[],
       mexErrMsgIdAndTxt("readLua:LuaError",
               "Could not load file %s.", cfgName);
    }
-   
+
    /* Execute file */
    if (lua_pcall(L, 0, 0, 0)) {
       lua_close(L);
       mexErrMsgIdAndTxt("readLua:LuaError",
               "Could not execute file %s.", cfgName);
    }
-   
+
    /* *******************************************************************
     * *** Getting parameters ********************************************
-    * ******************************************************************/   
-   
+    * ******************************************************************/
+
    size_t i;
    mxArray* mySymbol;
    char* symbol;
-   
+
    /* Loop over all symbols which should be read */
    for (i=0; i<nSymbols; i++) {
       /* Get the symbol name */
       mySymbol = mxGetCell(prhs[1], i);
-      
+
       /* Check if symbol name is an actual string */
       mrows = mxGetM(mySymbol);
       ncols = mxGetN(mySymbol);
@@ -347,31 +356,31 @@ void mexFunction( int nlhs, mxArray *plhs[],
          mexErrMsgIdAndTxt( "readLua:invalidInput", /* TODO*/
                "Second input must be a nonempty cell array which contains only nonempty strings.");
       }
-      
+
       /* Extract string */
       symbol = (char*) mxCalloc(ncols+1, sizeof(char));
       if (mxGetString(mySymbol, symbol, ncols+1) != 0) {
          mexErrMsgIdAndTxt( "readLua:couldNotGetString", /* TODO */
                "Could not get string.");
       }
-      
+
       /* Get the specified symbol (=key) from the lua config file and
        * add an appropriate field to the out array */
       myLuaGetByKeyAndAddFieldToOut(L, out, symbol);
-      
+
       /* Deallocate symbol */
       mxFree(symbol);
    }
-   
+
    /* *******************************************************************
     * *** Finish ********************************************************
     * ******************************************************************/
-   
+
    /* Close lua */
    lua_close(L);
-   
+
    /* Assign output */
    plhs[0] = out;
-   
+
    return;
 }
